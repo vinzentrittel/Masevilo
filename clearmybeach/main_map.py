aac913a59417b23ad26776944aaba607f9b48ad5
Vinzent Rittel <vinzentrittel@uni-koblenz.de>
2020-06-07 14:48:27 +0200

    Add top bar for navigation in main view.

--- clearmybeach/main_map.py
+++ clearmybeach/main_map.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 def create_blueprint():
     map_bp = Blueprint('map', __name__, url_prefix='/map')
 
     @map_bp.route('/')
     def index():
-        return 'Main view'
+        return render_template('map.html')
 
     return map_bp
